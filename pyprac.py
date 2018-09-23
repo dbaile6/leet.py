cc105b552cf1dda9278673e98fa153a5d2d3eb56
Dylan Bailey <dylanba1@gmail.com>
2018-09-23 16:43:29 -0500

    Dictionaries

--- pyprac.py
+++ pyprac.py
@@ -1,8 +1,28 @@
-age = int(input("What's your age?"))
+# age = int(input("What's your age?"))
 
-if age >= 18:
-    print ("Access allowed")
-elif age < 18 and age > 0:
-    print("Access not allowed")
-else:
-    print("Invalid age")    
+# if age >= 18:
+#     print ("Access allowed")
+# elif age < 18 and age > 0:
+#     print("Access not allowed")
+# else:
+#     print("Invalid age")    
+
+my_list = [1, 2, 3, "python", 67, [4, 5]]
+
+for j in my_list:
+    print j
+
+    # create a dictionary
+person = {
+            "name": "Amos",
+            "age": 23,
+            "hobbies": ["Travelling", "Swimming", "Coding", "Music"]
+        }
+
+# iterate through the dict and print the keys
+for key in person:
+    print(key)
+
+# iterate through the dict's keys and print their values
+for key in person:
+    print(person[key])
